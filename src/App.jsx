0e6110ff6569bd088590c19e78eb56e6484e12b6
adrian <adrianwang911@gmail.com>
2023-09-09 14:13:38 +1000

    Update Nav bar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from 'react-bootstrap';
+import './App.css';
+import Navigation from './components/navbar/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation></Navigation>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navigation.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './Navigation.css';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+
+function Navigation() {
+  return (
+    <Navbar>
+      <Container>
+        <Col xs lg="4">
+          <Navbar.Brand href="#home">PricesPeoplePay</Navbar.Brand>
+        </Col>
+        <Col>
+          <Nav className="justify-content-end">
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#search">Search</Nav.Link>
+            <Nav.Link href="#car2Cash">Car2Cash</Nav.Link>
+            <Nav.Link href="#valuations">Valuations</Nav.Link>
+            <Nav.Link href="#blog">Blog</Nav.Link>
+            <Nav.Link href="#contact">Contact us</Nav.Link>
+          </Nav>
+        </Col>
+        <Col xs lg="2">
+          <Button>Sign in</Button>
+        </Col>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
